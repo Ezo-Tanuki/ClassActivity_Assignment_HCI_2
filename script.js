f9a7f5fab15751a4d7da945d265e06338c178462
Ëzo-Tanuki <michailbryanlie@gmail.com>
2022-12-20 18:34:18 +0700

    update

--- script.js
+++ script.js
@@ -2,25 +2,14 @@
 $(() => {
     $(".text-contents").hide();
 
-    var $lesson_icon = $(".lesson-icon");
-    // console.log($lesson);
-    $lesson_icon.each(() => {
-        var ref = this
-        $(ref).mouseenter(() => {
-            console.log(this)
-            $(this).parent().children(".text-contents").fadeIn()
+    $(".lesson").each(function() {
+
+        $(this).children(".lesson-icon").mouseenter(function() {
+            $(this).siblings(".text-contents").fadeIn();
         })
-    })
 
-    // $(".lesson-icon").mouseenter((self) => {
-    //     // this.parent().children("p").fadeIn();
-    //     // $("img", this).fadeOut();
-    //     $(".text-contents").fadeIn();
-    // })
-
-    // $(".lesson-icon").mouseleave(() => {
-    //     $(".text-contents").fadeOut();
-    //     // $("img", this).fadeOut();
-    //     // $(".text-contents").fadeIn();
-    // })
-})
+        $(this).children(".lesson-icon").mouseleave(function() {
+            $(this).siblings(".text-contents").fadeOut();
+        });
+    });
+});
