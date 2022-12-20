e700160a59328e0c78e8b6fe8ca94ee6d7424a32
Ezo-Tanuki <michailbryanlie@gmail.com>
2022-12-20 18:01:15 +0700

    test

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Add your answer here
+$(() => {
+    $(".text-contents").hide();
+
+    var $lesson_icon = $(".lesson-icon");
+    // console.log($lesson);
+    $lesson_icon.each(() => {
+        var ref = this
+        $(ref).mouseenter(() => {
+            console.log(this)
+            $(this).parent().children(".text-contents").fadeIn()
+        })
+    })
+
+    // $(".lesson-icon").mouseenter((self) => {
+    //     // this.parent().children("p").fadeIn();
+    //     // $("img", this).fadeOut();
+    //     $(".text-contents").fadeIn();
+    // })
+
+    // $(".lesson-icon").mouseleave(() => {
+    //     $(".text-contents").fadeOut();
+    //     // $("img", this).fadeOut();
+    //     // $(".text-contents").fadeIn();
+    // })
+})
